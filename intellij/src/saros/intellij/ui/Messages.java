594b13b48af028ecaee342adea7ff61377998ccc
Tobias Bouschen <tobias.bouschen@googlemail.com>
2020-11-09 17:03:41 +0100

    [UI][I] Add option to kick user from session
    Adds the option for the host to kick users from the session.
    
    Adds an icon to the pop-up follow mode option.
    
    Moves the messages shown to the user in SessionPopMenu to
    IntellijMessages.properties.
    
    Copies the icon to kick a session participant from the Eclipse package.
    
    Resolves part of #1057.

--- intellij/src/saros/intellij/ui/Messages.java
+++ intellij/src/saros/intellij/ui/Messages.java
@@ -153,6 +153,9 @@ public class Messages {
 
   public static String Contact_saros_message_conditional;
 
+  public static String SessionPopMenu_follow_user;
+  public static String SessionPopMenu_kick_user;
+
   public static String ContactPopMenu_add_to_session_popup_text;
   public static String ContactPopMenu_start_session_popup_text;
   public static String ContactPopMenu_menu_tooltip_share_module;
