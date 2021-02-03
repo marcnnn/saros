2eb7e744569079a66455303e2abf0acf99b6e178
Justus Tartz <git@jrtberlin.de>
2021-02-03 12:18:12 +0100

    [UI][S] add list-sessions command to server cli

---
+++ server/src/saros/server/console/ListSessionsCommand.java
@@ -0,0 +1,43 @@
+package saros.server.console;
+
+import java.io.PrintStream;
+import java.util.List;
+import org.apache.log4j.Logger;
+import saros.session.ISarosSessionManager;
+
+public class ListSessionsCommand extends ConsoleCommand {
+  private static final Logger log = Logger.getLogger(ListSessionsCommand.class);
+  private final ISarosSessionManager sessionManager;
+
+  public ListSessionsCommand(ISarosSessionManager sessionManager, ServerConsole console) {
+    this.sessionManager = sessionManager;
+    console.registerCommand(this);
+  }
+
+  @Override
+  public String identifier() {
+    return "list-sessions";
+  }
+
+  @Override
+  public int minArgument() {
+    return 0;
+  }
+
+  @Override
+  public String help() {
+    return "list-sessions - List all active sessions";
+  }
+
+  @Override
+  public void execute(List<String> args, PrintStream out) {
+    try {
+      out.println(
+          sessionManager.getSession().getID()
+              + " "
+              + sessionManager.getSession().getUsers().toString());
+    } catch (Exception e) {
+      log.error("Error getting sessions", e);
+    }
+  }
+}
